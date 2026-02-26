43b6228ef53c4b276358c171e6be4c2130b1cd77
HarshSeth1209 <harshsethpk007@gmail.com>
2026-02-27 02:04:15 +0530

    Configure frontend for production backend

--- hrms-frontend/src/api/api.js
+++ hrms-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://hrms-backend-zg8t.onrender.com/api",
 });
 
 export default api;
